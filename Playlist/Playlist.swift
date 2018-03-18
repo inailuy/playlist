a8b7ff9724dcc317adfbc0f6fb80cd7e6cc69439
yuliani <inailuy@me.cm>
2018-03-17 20:12:11 -0400

    deleted comments from Playlist

--- Playlist/Playlist.swift
+++ Playlist/Playlist.swift
@@ -15,37 +15,6 @@ struct Playlist: Decodable {
     let next :String
     let total :Int
     let items :[Item]
-    
-//    private enum CodingKeys: String, CodingKey {
-//        case limit
-//        case href
-//        case previous
-//        case offset
-//        case next
-//        case total
-//        case items
-//    }
-//
-//    init(limit: Int, href: String, previous: String, offset: Int, next: String, total: Int, items:Array<Any>) {
-//        self.limit = limit
-//        self.href = href
-//        self.previous = previous
-//        self.offset = offset
-//        self.next = next
-//        self.total = total
-//        self.items = items
-//    }
-//
-//    init(from decoder:Decoder) throws {
-//        let values = try decoder.container(keyedBy: CodingKeys.self)
-//        limit = try values.decode(Int.self, forKey: .limit)
-//        href = try values.decode(String.self, forKey: .href)
-//        //previous = try values.decode(String.self, forKey: .previous)
-//        offset = try values.decode(Int.self, forKey: .offset)
-//        next = try values.decode(String.self, forKey: .next)
-//        total = try values.decode(Int.self, forKey: .total)
-//        //items = try values.decode(Array<Any>.self, forKey: .items)
-//    }
 }
 
 struct Item: Codable {
@@ -53,9 +22,4 @@ struct Item: Codable {
     var id :String
     var name :String
     var snapshot_id :String
-    //var type :String
-    //var publicString :Bool
-    //var collaborative :Bool
-    //var uri :String
 }
-
